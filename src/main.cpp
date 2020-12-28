43747d5654d5ec85ca103b9fa1319b45874b64d1
kraken-lab <64556998+kraken-lab@users.noreply.github.com>
2020-12-28 18:55:07 +0100

    Get DHT22 working at digital pin 5

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,49 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include <dht.h>
+
+dht DHT;
+
+#define DHT22_PIN 5
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("DHT TEST PROGRAM ");
+  Serial.print("LIBRARY VERSION: ");
+  Serial.println(DHT_LIB_VERSION);
+  Serial.println();
+  Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  // READ DATA
+  Serial.print("DHT12, \t");
+  int chk = DHT.read22(DHT22_PIN);
+
+  switch (chk)
+  {
+    case DHTLIB_OK:  
+		Serial.print("OK,\t"); 
+		break;
+    case DHTLIB_ERROR_CHECKSUM: 
+		Serial.print("Checksum error,\t"); 
+		break;
+    case DHTLIB_ERROR_TIMEOUT: 
+		Serial.print("Time out error,\t"); 
+		break;
+    default: 
+		Serial.print("Unknown error,\t"); 
+		break;
+  }
+  // DISPLAY DATA
+  Serial.print(DHT.humidity, 1);
+  Serial.print(",\t\t");
+  Serial.println(DHT.temperature, 1);
+
+  delay(2000);
 }
+//
+// END OF FILE
+//
