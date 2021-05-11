628017496718b9ce843bdf2105ca3d467c59e977
kraken-lab <64556998+kraken-lab@users.noreply.github.com>
2021-05-11 14:59:55 +0200

    fix periodically temp + hum transmission

--- include/main.h
+++ include/main.h
@@ -2,4 +2,8 @@
 
 String prepareAnswer(String command);
 String addDelimiter(String input);
+String addStartEndMarkers(String input);
 void checkEspCommands();
+void sendWithStartEndMarkers(String data);
+void recvWithStartEndMarkers();
+void showNewData();
--- src/main.cpp
+++ src/main.cpp
@@ -13,18 +13,22 @@ dht DHT;
 const int _serialRx = 2;
 const int _serialTx = 3;
 
-SoftwareSerial esp = SoftwareSerial(_serialRx, _serialTx);
+SoftwareSerial esp(_serialRx, _serialTx);
+
+const byte numChars = 32;
+char receivedChars[numChars];
+
+boolean newData = false;
 
 String prepareAnswer(String command){
-  if (command==CMD_TEMP)
-  {
-    return addDelimiter(String(DHT.temperature));
-  } else if (command == CMD_HUM)
-  {
-    return addDelimiter(String(DHT.humidity));
-  } else if (command == CMD_SOIL_1)
-  {
+  if (command.equals(CMD_TEMP)) {
+    return String(DHT.temperature);
+  } else if (command.equals(CMD_HUM)) {
+    return String(DHT.humidity);
+  } else if (command.equals(CMD_SOIL_1)) {
     /* code */
+  } else {
+   return "error: no valid command found";
   }
 }
 
@@ -32,27 +36,87 @@ String addDelimiter(String input) {
   return input+":";
 }
 
+String addStartEndMarkers(String input) {
+  return "<"+input+">";
+}
+
 void checkEspCommands() {
-  if (esp.available() && esp.peek())
-  {
-    String command = esp.readString();
 
-    String answer = prepareAnswer(command);
+    String receivedString = receivedChars;
+    Serial.println("command: "+receivedString);
 
-    esp.write(answer.c_str());
+    String answer = prepareAnswer(receivedString);
+    Serial.println("answer: "+answer);
 
-  }
+    sendWithStartEndMarkers(answer);
+}
+
+void sendWithStartEndMarkers(String data) {
+  data = addStartEndMarkers(data);
+  esp.write(data.c_str());
+}
+
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+    
+    Serial.println("reading one data chunk from buffer (within < and >)");
+    Serial.println();
+    while (esp.available() > 0 ) {
+        rc = esp.read();
+        Serial.print(rc);
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+                //Serial.println("reached endmarker");
+                break;
+            }
+        }
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+    Serial.println();
+}
+
+void showNewData() {
+    if (newData == true) {
+        Serial.print("This just in ... ");
+        Serial.println(receivedChars);
+        newData = false;
+    }
 }
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println("DHT TEST PROGRAM ");
+  Serial.begin(SERIAL_SPEED);
+  
+  Serial.println("GREEN ONE");
   Serial.print("LIBRARY VERSION: ");
   Serial.println(DHT_LIB_VERSION);
   Serial.println();
   Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)");
+  
   esp.begin(SERIAL_SPEED);
+  while(!esp) {
+    Serial.println("esp software-serial not available");
+    esp.begin(SERIAL_SPEED);
+  }
+  esp.listen();
 }
 
 void loop()
@@ -81,10 +145,12 @@ void loop()
   Serial.print(",\t\t");
   Serial.println(DHT.temperature, 1);
 
-  delay(2000);
+  delay(1000);
 
-  checkEspCommands();
-  
+  while (esp.available() != 0) {
+    recvWithStartEndMarkers();
+    checkEspCommands();
+  }
 }
 //
 // END OF FILE
